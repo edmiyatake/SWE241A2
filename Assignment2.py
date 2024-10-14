b9b46c7adaed94371f790f9cfea5636282f7a526
Edwin Miyatake <edmiyatake@gmail.com>
2024-10-13 19:55:16 -0700

    completed the binary search to fit the conditions

--- Assignment2.py
+++ Assignment2.py
@@ -1,18 +1,48 @@
-nums = [17,4,9,10,13,17,17,19,21]
-target = 17
+# must be O(log(n)) therefore binary search
+def firstInstance(nums,target):
+    left,right,sol = 0,len(nums) - 1,0
+    while left <= right:
+        mid = (left + right) // 2
+        if target > nums[mid]:
+        # if the target is in the upper half of the array
+        # change left pointer
+            left = mid + 1
+        elif target < nums[mid]:
+            right = mid - 1
+        # if the target is in the lower half of the array
+        # change right pointer
+        else:
+            sol = mid
+            # but to guarantee that this is the first instance we keep should keep going down
+            right = mid - 1
+        # this should be the first instance?
+    return sol
+
+def lastInstance(nums,target):
+    left,right,sol = 0,len(nums) - 1,0
+    while left <= right:
+        mid = (left + right) // 2
+        if target > nums[mid]:
+        # if the target is in the upper half of the array
+        # change left pointer
+            left = mid + 1
+        elif target < nums[mid]:
+            right = mid - 1
+        # if the target is in the lower half of the array
+        # change right pointer
+        else:
+            sol = mid
+            # but to guarantee that this is the last instance we keep should keep going up
+            left = mid + 1
+
+    return sol
+
+nums = [1,4,9,10,13,17,17,19,21]
+nums2 = [12,45,98,102,102,102,179,192,210]
+# first couple of bugs were because of the not sorted array => yikes
+target = 102
+
+print(f"This is the first instance: {firstInstance(nums2,target)}")
+print(f"This is the last instance: {lastInstance(nums2,target)}")
 
 
-# must be O(log(n))
-def search(nums,target):
-    sol1,sol2 = None,None
-    for i in range(0,len(nums) - 1):
-        if nums[i] == target:
-            sol1 = i
-            break
-    for i in range(len(nums) - 1,0,-1):
-        if nums[i] == target:
-            sol2 = i
-            break
-    return sol1,sol2
-
-print(search(nums,target))
