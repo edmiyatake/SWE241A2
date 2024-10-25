8578ec95390878977844ddf31267a187c543e16b
Edwin Miyatake <122956602+edmiyatake@users.noreply.github.com>
2024-10-25 12:30:25 -0700

    removed unneccessary  comments

--- Assignment2p2.py
+++ Assignment2p2.py
@@ -11,26 +11,17 @@ def binarySearch(matrix,target):
     # what would mid be so that i can traverse both vertically and laterally in a matrix array
     # can i assume that every column array is the same size?
     row = len(matrix)
-    # only works if n array size is fixed if not then it has to be O(n) to iterate through the different sizes
     col = len(matrix[0])
-    # i need to know the total number of elements in the matrix from 0 to len(matrix2D)
     len1d = row * col - 1
-    # now I can start the binary search to achieve log(m*n) time
     left,right = 0,len1d
     while left <= right:
         mid = (left + right) // 2
-        # i need a sway to split the arrays into row/col
-        # first iteration would be the max len of 2d matrix by the number of columns to get rows
         currRow = mid // col
-        # columns is just the max length of the 2d matrix mod the size of the column
         currCol = mid % col
-        # first if the target is in the upper bracket
         if target > matrix[currRow][currCol]:
             left = mid + 1
-        # if the target is in the lower bracket
         elif target < matrix[currRow][currCol]:
             right = mid - 1
-        # finally if the target is at the perfect place
         else:
             return True
     
