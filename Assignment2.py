ed6b60d47ee817e898532e1025fed0742b76ec54
Edwin Miyatake <122956602+edmiyatake@users.noreply.github.com>
2024-11-11 13:42:51 -0800

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -40,38 +40,40 @@ def lastInstance(nums,target):
 def task1(nums,target):
     return [firstInstance(nums,target),lastInstance(nums,target)]
 
-print("Assignment Examples")
-nums = [4,9,10,13,17,17,19,21]
-target = 17
-print(f"nums = {nums}, target = {target} ")
-print(f"Solution is {task1(nums,target)}\n")
-nums1 = [2,4,6,8,10,14,16]
-target1 = 12
-print(f"nums = {nums1}, target = {target1} ")
-print(f"Solution is {task1(nums1,target1)}\n")
-nums2 = []
-target2 = 0
-print(f"nums = {nums2}, target = {target2} ")
-print(f"Solution is {task1(nums2,target2)}\n")
+# print("Assignment Examples")
+# nums = [4,9,10,13,17,17,19,21]
+# target = 17
+# print(f"nums = {nums}, target = {target} ")
+# print(f"Solution is {task1(nums,target)}\n")
+# nums1 = [2,4,6,8,10,14,16]
+# target1 = 12
+# print(f"nums = {nums1}, target = {target1} ")
+# print(f"Solution is {task1(nums1,target1)}\n")
+# nums2 = []
+# target2 = 0
+# print(f"nums = {nums2}, target = {target2} ")
+# print(f"Solution is {task1(nums2,target2)}\n")
 
-print("Custom Test Cases")
-print("Test both edges of the array")
-print(f"nums = {[42,2,25,65,42]}, target = {42} ")
-print(f"Solution is {task1([42,2,25,65,42],42)}\n")
+# print("Custom Test Cases")
+# print("Test both edges of the array")
+# print(f"nums = {[42,2,25,65,42]}, target = {42} ")
+# print(f"Solution is {task1([42,2,25,65,42],42)}\n")
 
-print("Test one target instance in the array")
-print(f"nums = {[22,2,25,65,76]}, target = {25} ")
-print(f"Solution is {task1([22,2,25,65,76],25)}\n")
+# print("Test one target instance in the array")
+# print(f"nums = {[22,2,25,65,76]}, target = {25} ")
+# print(f"Solution is {task1([22,2,25,65,76],25)}\n")
 
-print("Test if target is not found")
-print(f"nums = {[22,2,25,65,76]}, target = {72} ")
-print(f"Solution is {task1([22,2,25,65,76],72)}\n")
+# print("Test if target is not found")
+# print(f"nums = {[22,2,25,65,76]}, target = {72} ")
+# print(f"Solution is {task1([22,2,25,65,76],72)}\n")
 
-print("Test two targets in the back of the array")
-print(f"nums = {[22,2,25,65,76,97,97]}, target = {97} ")
-print(f"Solution is {task1([22,2,25,65,76,97,97],97)}\n")
+# print("Test two targets in the back of the array")
+# print(f"nums = {[22,2,25,65,76,97,97]}, target = {97} ")
+# print(f"Solution is {task1([22,2,25,65,76,97,97],97)}\n")
 
-print("Test empty array")
-print(f"nums = {[]}, target = {2} ")
-print(f"Solution is {task1([],2)}\n")
+# print("Test empty array")
+# print(f"nums = {[]}, target = {2} ")
+# print(f"Solution is {task1([],2)}\n")
 
+
+print(task1([2,2,2,2,2,2,2],2))
